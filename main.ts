cccd293093609985f1a1f70102e35bbbfd4d52eb
guest13otto <114576944+guest13otto@users.noreply.github.com>
2022-10-14 14:24:13 +0800

    STARTED THE THING AGGGG

--- main.ts
+++ main.ts
@@ -5,5 +5,21 @@ input.onButtonPressed(Button.B, function () {
 	
 })
 input.onGesture(Gesture.Shake, function () {
-	
+    music.playTone(523, music.beat(BeatFraction.Whole))
+    music.playTone(622, music.beat(BeatFraction.Whole))
+    music.playTone(698, music.beat(BeatFraction.Half))
+    music.playTone(698, music.beat(BeatFraction.Quarter))
+    music.playTone(622, music.beat(BeatFraction.Quarter))
+    music.playTone(622, music.beat(BeatFraction.Half))
+    music.playTone(698, music.beat(BeatFraction.Half))
+    music.playTone(698, music.beat(BeatFraction.Half))
+    music.playTone(698, music.beat(BeatFraction.Half))
+    music.playTone(932, music.beat(BeatFraction.Half))
+    music.playTone(831, music.beat(BeatFraction.Half))
+    music.playTone(784, music.beat(BeatFraction.Quarter))
+    music.playTone(698, music.beat(BeatFraction.Half))
+    music.playTone(784, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(784, music.beat(BeatFraction.Whole))
+    music.playTone(932, music.beat(BeatFraction.Whole))
 })
